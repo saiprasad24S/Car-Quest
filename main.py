229cb0a68c05d1df12bd979ada8972cf077af711
saikrishna-avskr <saikrishnaavskr@gmail.com>
2025-03-03 19:55:32 +0530

    made changes to deploy project.

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template, jsonify, request
 from flask_mysqldb import MySQL
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 mysql = MySQL(app)
 
-# Configure MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '15313037S@i'
-app.config['MYSQL_DB'] = 'car_quest'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
+app.config['MYSQL_CURSORCLASS'] = os.getenv('MYSQL_CURSORCLASS')
+app.config['MYSQL_SSL_CA'] = os.getenv('MYSQL_SSL_CA')
 
 @app.route('/')
 def index():
